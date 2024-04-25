44b7981e7a2a18e8254df208d95cad2732f3a1f7
Vicente-McMahon <vicente.mcmahon@gmail.com>
2024-04-25 21:30:44 +0100

    Added a comment.

--- src/global/global.hpp
+++ src/global/global.hpp
@@ -3,6 +3,6 @@
 
 struct Application;
 // Extern allows other files to know about global variables stored else where.
-extern Application application;
+extern Application application; // This is defined in 'main.cpp'.
 extern SDL_Rect testRectangle;
 extern SDL_DisplayMode displayMode;
