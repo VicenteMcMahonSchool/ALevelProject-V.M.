8fd4c2186213a92e63c797638030106a5bcdd70b
Vicente-McMahon <vicente.mcmahon@gmail.com>
2024-05-04 20:25:01 +0100

    Made 'TileMap' a valid 'GameObject'.

--- src/game_objects/game_object/game_object.hpp
+++ src/game_objects/game_object/game_object.hpp
@@ -1,17 +1,19 @@
 #pragma once
-#include "../rectangle/rectangle.hpp"
 #include "../movable_rectangle/movable_rectangle.hpp"
+#include "../tile_map/tile_map.hpp"
 
 enum GAME_OBJECT_TYPE
 {
     RECTANGLE,
-    MOVABLE_RECTANGLE
+    MOVABLE_RECTANGLE,
+    TILE_MAP
 };
 
 union GameObjectUnion
 {
     Rectangle rectangle;
     MovableRectangle movableRectangle;
+    TileMap tileMap;
 };
 
 class GameObject
