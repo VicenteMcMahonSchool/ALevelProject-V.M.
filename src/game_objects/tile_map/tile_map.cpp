d6326e17dceaf79143574d3751c0c0a4de3025a2
Vicente-McMahon <vicente.mcmahon@gmail.com>
2024-05-21 18:32:12 +0100

    Reverted debugging colours.

--- src/game_objects/tile_map/tile_map.cpp
+++ src/game_objects/tile_map/tile_map.cpp
@@ -21,16 +21,16 @@ void TileMap::draw(void)
     // SDL_SetRenderDrawColour(renderer, this->colour.r, this->colour.g, this->colour.b, this->colour.a); // Sets draw colour.
     for (unsigned int i = 0; i < NUMBER_OF_TILES; i++)
     {
-        if (i % 3 == 0)
-            SDL_SetRenderDrawColour(renderer, 0XFF, 0X33, 0X33, 0X00);
-        else if (i % 3 == 1)
-            SDL_SetRenderDrawColour(renderer, 0X33, 0XFF, 0X33, 0X00);
-        else if (i % 3 == 2)
-            SDL_SetRenderDrawColour(renderer, 0X33, 0X33, 0XFF, 0X00);
-        // if (this->tileMap[i] == TILE_AIR)
-        //     SDL_SetRenderDrawColour(renderer, 0X33, 0X33, 0X33, 0X00);
-        // else if (this->tileMap[i] == TILE_PLATFORM)
-        //     SDL_SetRenderDrawColour(renderer, 0X33, 0XDD, 0X33, 0XFF);
+        // if (i % 3 == 0)
+        //     SDL_SetRenderDrawColour(renderer, 0XFF, 0X33, 0X33, 0X00);
+        // else if (i % 3 == 1)
+        //     SDL_SetRenderDrawColour(renderer, 0X33, 0XFF, 0X33, 0X00);
+        // else if (i % 3 == 2)
+        //     SDL_SetRenderDrawColour(renderer, 0X33, 0X33, 0XFF, 0X00);
+        if (this->tileMap[i] == TILE_AIR)
+            SDL_SetRenderDrawColour(renderer, 0X33, 0X33, 0X33, 0X00);
+        else if (this->tileMap[i] == TILE_PLATFORM)
+            SDL_SetRenderDrawColour(renderer, 0X33, 0XDD, 0X33, 0XFF);
         SDL_RenderFillRect(renderer, this->rectangles + i); // Fill rectangle.
     }
     GeneralGameObject::draw();
