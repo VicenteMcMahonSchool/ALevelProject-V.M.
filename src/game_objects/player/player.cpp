5240a0368c8c13986a58bdefda0549da9b670493
Vicente-McMahon <vicente.mcmahon@gmail.com>
2024-05-21 18:54:12 +0100

    Added 'Player'.

---
+++ src/game_objects/player/player.cpp
@@ -0,0 +1,3 @@
+#include "./player.hpp"
+
+Player::Player(Vector2 position) : MovableRectangle(position, {0X33, 0X33, 0X77, 0XFF}, 120, 120) {}
---
+++ src/game_objects/player/player.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "../movable_rectangle/movable_rectangle.hpp"
+
+class Player : public MovableRectangle
+{
+    Player(Vector2 position);
+};
