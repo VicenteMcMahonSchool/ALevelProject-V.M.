77b371085e5be9e1a03201f306fdd5e69ed108a4
Vicente-McMahon <vicente.mcmahon@gmail.com>
2024-07-09 09:31:16 +0100

    Made velocity decrease each frame.

--- src/game_objects/player/player.cpp
+++ src/game_objects/player/player.cpp
@@ -3,14 +3,15 @@
 Player::Player(Vector2 position) : MovableRectangle(position, {0X33, 0X33, 0X77, 0XFF}, 120, 120) {}
 void Player::update(double deltaTime)
 {
+    this->velocity /= 1.05;
     Vector2 deltaVelocity{0, 0};
     if (isKeyDown(SDL_SCANCODE_A))
         deltaVelocity.x -= 1;
-    else if (isKeyDown(SDL_SCANCODE_D))
+    if (isKeyDown(SDL_SCANCODE_D))
         deltaVelocity.x += 1;
-    else if (isKeyDown(SDL_SCANCODE_W))
+    if (isKeyDown(SDL_SCANCODE_W))
         deltaVelocity.y -= 1;
-    else if (isKeyDown(SDL_SCANCODE_S))
+    if (isKeyDown(SDL_SCANCODE_S))
         deltaVelocity.y += 1;
     double length = deltaVelocity.length();
     if (length != 0)
