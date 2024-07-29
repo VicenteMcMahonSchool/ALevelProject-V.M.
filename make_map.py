7b166b77226203f64c2fb8e9d4b4a8eaa586f018
Vicente-McMahon <vicente.mcmahon@gmail.com>
2024-07-29 19:26:57 +0100

    Made it so the bottom of the map is the lose tile.

--- make_map.py
+++ make_map.py
@@ -16,10 +16,12 @@ if __name__ == "__main__":
     with open("./map", "wb") as file:
         buffer = b""
         for i in range(WIDTH_OF_TILE_MAP * HEIGHT_OF_TILE_MAP):
-            if i < WIDTH_OF_TILE_MAP or i > NUMBER_OF_TILES - WIDTH_OF_TILE_MAP:
+            if i < WIDTH_OF_TILE_MAP:
                 buffer += tileTypes["TILE_BOARDER"]
             elif i % WIDTH_OF_TILE_MAP == 0 or i % WIDTH_OF_TILE_MAP == WIDTH_OF_TILE_MAP-1:
                 buffer += tileTypes["TILE_BOARDER"]
+            elif i > NUMBER_OF_TILES - WIDTH_OF_TILE_MAP:
+                buffer += tileTypes["TILE_LOSE"]
             else:
                 buffer += tileTypes["TILE_AIR"]
         file.write(buffer)
