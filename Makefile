d372ae8e6b88c4aa3d5985980f118daffa80aa00
Vicente-McMahon <vicente.mcmahon@gmail.com>
2024-08-06 11:02:34 +0100

    Changed the method of compiling as I am now doing it on a ubuntu container. This meant that I had to modify the 'Makefile' and recompile everything.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-COMPILER=g++-14
-FLAGS=`/home/linuxbrew/.linuxbrew/bin/sdl2-config --cflags --libs` -std=c++23 -ldl -lm -lSDL2_image -lSDL2_ttf
+COMPILER=g++
+FLAGS=`sdl2-config --cflags --libs` -std=c++23 -ldl -lm -lSDL2_image -lSDL2_ttf
 SRC_DIR=./src
 OBJ_DIR=./obj
 SRCS=$(shell find ${SRC_DIR} -name *.cpp -not -type d | xargs echo)
---
+++ obj/game_objects/general_game_object/general_game_object.d
@@ -0,0 +1,5 @@
+obj/game_objects/general_game_object/general_game_object.o: \
+ src/game_objects/general_game_object/general_game_object.cpp \
+ src/game_objects/general_game_object/./general_game_object.hpp \
+ src/game_objects/general_game_object/./../../vector2/vector2.hpp \
+ src/game_objects/general_game_object/./../../macros/macros.hpp
